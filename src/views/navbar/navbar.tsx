7d5b80387dcc6c1928d8c4f0867f75b60d0de622
Vinicius <vinii.d3v@gmail.com>
2026-09-12 23:53:24 -0300

    navbar complete

--- src/views/navbar/navbar.tsx
+++ src/views/navbar/navbar.tsx
@@ -2,11 +2,11 @@ import './navbar.css'
 
 export function Navbar() {
     return (
-        <section className='navbar px-5'>
+        <section className='navbar px-3 px-md-5'>
             <nav className='w-100'>
-                <div className="container-fluid d-flex justify-content-between align-items-center">
-                    <h2>LivroDex</h2>
-                    <form className="d-flex" role="search">
+                <div className="container-fluid d-flex justify-content-between align-items-center ">
+                    <h2 className='mt-2'>LivroDex</h2>
+                    <form className="search d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Buscar livro ou autor..." aria-label="Search" />
                     </form>
                 </div>
