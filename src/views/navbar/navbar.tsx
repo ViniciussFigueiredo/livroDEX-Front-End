caa60f478441a82286dba871f61b44eed18935ba
Vinicius <vinii.d3v@gmail.com>
2026-09-12 23:30:27 -0300

    navbar

--- src/views/navbar/navbar.tsx
+++ src/views/navbar/navbar.tsx
@@ -1,9 +1,16 @@
 import './navbar.css'
 
-export function Navbar () {
+export function Navbar() {
     return (
-        <div>
-           
-        </div>
+        <section className='navbar px-5'>
+            <nav className='w-100'>
+                <div className="container-fluid d-flex justify-content-between align-items-center">
+                    <h2>LivroDex</h2>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Buscar livro ou autor..." aria-label="Search" />
+                    </form>
+                </div>
+            </nav>
+        </section>
     )
 }
