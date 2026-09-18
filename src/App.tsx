e5b45c1199f4951eb14a216e32453790e27e0b53
Vinicius <vinii.d3v@gmail.com>
2026-09-17 22:28:37 -0300

    autor page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Book } from "./views/book-page/book"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main } from "./views/main-page/main"
+import { Autor } from "./views/autor-page/autor";
 
 function App() {
 
   return (
     <BrowserRouter >
       <Routes>
-        <Route path="/" element={<Main />}/> 
+        <Route path="/dasdasd" element={<Main />}/> 
         <Route path="/livros/:titulo" element={<Book />}/>
+        <Route path="/" element={<Autor />}/>
       </Routes>
     </BrowserRouter>
     
--- src/views/autor-page/autor.tsx
+++ src/views/autor-page/autor.tsx
@@ -1,9 +1,51 @@
+import { Suggestions } from '../../components/books-suggestions/suggestions'
+import { Infos } from '../../components/infos/infos'
+import { Navbar } from '../navbar/navbar'
 import './autor.css'
 
-export function Autor () {
+export function Autor() {
     return (
-        <div>
-            
-        </div>
+        <section className='autor-main'>
+            <Navbar />
+            <div className='w-100 px-5 d-flex justify-content-center mt-5'>
+                <div className='autor-details d-flex justify-content-around px-4 py-3'>
+                    <div className='w-25 d-flex justify-content-center align-items-center'>
+                        <img src="" alt="" />
+                    </div>
+                    <div className='w-75 mt-3'>
+                        <h1>Machado de assis</h1>
+                        <div className='d-flex gap-2'>
+                            <Infos
+                                text={`Nascimento: `}
+                                cor='#E2E8F0'
+                                textoCor='#475569'
+                            />
+                            <Infos
+                                text={`Falecimento: `}
+                                cor='#E2E8F0'
+                                textoCor='#475569'
+                            />
+                            <Infos
+                                text={`Obras: `}
+                                cor='#E0F2FE'
+                                textoCor='#0284C7'
+                            />
+                        </div>
+                        <p className='mt-3'>Biografia:</p>
+                        <div className='biografia w-100 p-3'>
+                            <p>adadsdasdas</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='w-100 px-5 d-flex justify-content-center mt-5'>
+                <div className='autor-books'>
+                    <h2>Top 5 Obras do Autor</h2>
+                    <Suggestions />
+                </div>
+            </div>
+
+
+        </section>
     )
 }
